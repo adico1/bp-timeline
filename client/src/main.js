5f38afef931a1ad8aabe2740324c733a67c7dc5b
Adi Cohen <adico1@gmail.com>
2020-06-15 11:02:16 +0300

    first commit

--- client/src/main.js
+++ client/src/main.js
@@ -1,8 +1,14 @@
+import MagnifyIcon from 'vue-material-design-icons/Magnify.vue';
+import EyeOutlineIcon from 'vue-material-design-icons/EyeOutline.vue';
 import Vue from 'vue';
 import App from './App.vue';
 import router from './router';
+import 'normalize.css';
+import 'vue-material-design-icons/styles.css';
 
 Vue.config.productionTip = false;
+Vue.component('magnify-icon', MagnifyIcon);
+Vue.component('eye-outline-icon', EyeOutlineIcon);
 
 new Vue({
   router,
